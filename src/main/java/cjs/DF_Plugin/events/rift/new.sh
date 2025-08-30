52887d9d59514fbec7710ac6c88f36382078838e
didi78579 <cjscjs0527@gmail.com>
2025-08-31 02:51:50 +0900

    2.0 update_03

---
+++ src/main/java/cjs/DF_Plugin/events/rift/new.sh
@@ -0,0 +1,3 @@
+git config --global user.name "didi78579"
+git config --global user.email "cjscjs0527@gmail.com"git config --global user.name "didi78579"
+git config --global user.email "cjscjs0527@gmail.com"
